35803f368b943c2e25a123c844f89c9996ba88e2
Andrew Chan <chanandrew1925@gmail.com>
2026-09-19 18:48:31 -0700

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> records;
+        int sum = 0;
+        for (auto op : operations) {
+            if (op == "+") {
+                int firstNum = records.top();
+                records.pop();
+                int secondNum = records.top();
+                records.push(firstNum);
+                int total = firstNum + secondNum;
+                records.push(total);
+                sum += total;
+            } else if (op == "C") {
+                sum -= records.top();
+                records.pop();
+            } else if (op == "D") {
+                sum += records.top() * 2;
+                records.push(records.top() * 2);
+            } else {
+                sum += stoi(op);
+                records.push(stoi(op));
+            }
+        }
+
+        return sum;
+    }
+};
